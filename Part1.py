93f87d5171e4d4d00711912906f75bdb276f8989
Polydefkis <polydefkis.d.gkagkos@gmail.com>
2019-01-22 18:40:39 +0100

    no message

--- Part1.py
+++ Part1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
 np.random.seed(0)
 
@@ -9,21 +11,31 @@ def create_linearly_separable_data():
     meanB = [-2.5, -2.5]
 
     sigmaA = [[1, 0], [0, 0.5]]
-    sigmaB = [[-1, 0], [0, 1]]
+    sigmaB = [[-1, 0], [0, -1]]
 
     n = 100
 
     classA = np.random.multivariate_normal(meanA, sigmaA, n)
+    labelsA = np.zeros((classA.shape[0], 1))
     classB = np.random.multivariate_normal(meanB, sigmaB, n)
+    labelsB = np.ones((classB.shape[0], 1))
 
-    return [classA, classB]
+    X = np.concatenate((classA, classB), axis=0)
+    Y = np.concatenate((labelsA, labelsB))
+
+    X, Y = shuffle(X, Y)
+    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    return [X, Y]
 
 
-[classA, classB] = create_linearly_separable_data()
+[X, Y] = create_linearly_separable_data()
+idx1 = np.where(Y == 0)
+idx2 = np.where(Y == 1)
+print(len(idx1))
 
-plt.scatter(classA[:, 0], classA[:, 1])
-plt.scatter(classB[:, 0], classB[:, 1])
+print(len(idx2))
+plt.scatter(X[idx1,0], X[idx1,1])
+plt.scatter(X[idx2,0], X[idx2,1])
 
 plt.axis('equal')
 plt.show()
-
