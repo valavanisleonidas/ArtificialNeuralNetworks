5f0f48eccb5cd6adfabe2d59d6d8d857f9f5bb28
Leonidas Valavanis <valavanisleonidas@gmail.com>
2019-01-22 18:11:53 +0100

    function it

--- Part1.py
+++ Part1.py
@@ -3,19 +3,27 @@ import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
-meanA = [2, 1.5]
-meanB = [-2.5, -2.5]
 
-sigmaA = [[1, 0], [0, 0.5]]
-sigmaB = [[-1, 0], [0, 1]]
+def create_linearly_separable_data():
+    meanA = [2, 1.5]
+    meanB = [-2.5, -2.5]
 
-n = 100
+    sigmaA = [[1, 0], [0, 0.5]]
+    sigmaB = [[-1, 0], [0, 1]]
 
-classA = np.random.multivariate_normal(meanA, sigmaA, n)
-classB = np.random.multivariate_normal(meanB, sigmaB, n)
+    n = 100
+
+    classA = np.random.multivariate_normal(meanA, sigmaA, n)
+    classB = np.random.multivariate_normal(meanB, sigmaB, n)
+
+    return [classA, classB]
+
+
+[classA, classB] = create_linearly_separable_data()
 
 plt.scatter(classA[:, 0], classA[:, 1])
 plt.scatter(classB[:, 0], classB[:, 1])
 
 plt.axis('equal')
 plt.show()
+
